bdbc02a0c9bfc6e78e54164c09903ec17124ace7
DIEGOROCU <62994170+DIEGOROCU@users.noreply.github.com>
2024-09-24 10:13:55 +0200

    Add files via upload

---
+++ 2425-Lemmings-main/src/tp1/logic/gameobjects/Lemming.java
@@ -0,0 +1,20 @@
+package tp1.logic.gameobjects;
+
+import tp1.logic.Game;
+
+public class Lemming {
+	//Atributos
+	Position posicion = new Position();
+	private boolean defunciont;
+	private WalkerRole rol;
+	private Game juego;
+	
+	//TODO fill your code
+	
+	/**
+	 *  Implements the automatic update	
+	 */
+	public void update() {
+		//TODO fill your code
+	}
+}
