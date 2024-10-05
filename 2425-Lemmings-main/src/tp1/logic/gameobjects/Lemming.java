b1538b3f7fb1975545780b82d3cd117b3a2c09c0
Pablo Pardo <pablo.pc2005@gmail.com>
2024-10-06 00:02:34 +0200

    Refactor Lemming class to track previous direction
    This commit refactors the Lemming class in order to track the previous direction of the lemming. A new attribute, previousDirection, has been added to the class. This attribute is updated whenever the lemming changes its direction. The purpose of this change is to allow the lemming to revert back to its previous direction if it encounters a wall while moving downwards. Additionally, the move() method has been modified to handle this logic.

--- 2425-Lemmings-main/src/tp1/logic/gameobjects/Lemming.java
+++ 2425-Lemmings-main/src/tp1/logic/gameobjects/Lemming.java
@@ -10,6 +10,7 @@ public class Lemming {
 	//Atributos
 	private Position pos;
 	private Direction direction;
+	private Direction previousDirection;
 	private boolean vivo;
 	private int fuerzaCaida; // Con 3 se muere
 	private WalkerRole rol;
@@ -20,6 +21,7 @@ public class Lemming {
 	public Lemming() {
 		this.pos = new Position();
 		this.direction = Direction.RIGHT;
+		this.previousDirection = Direction.NONE;
 		this.vivo = true;
 		this.fuerzaCaida = 0;
 		this.rol = new WalkerRole();
@@ -56,24 +58,38 @@ public class Lemming {
 	
 	// Mueve el lemming
 	public void move() {
+		// Si la direccion es hacia abajo
 		if (direction == Direction.DOWN) {
+			// Si hay una pared abajo
 			if (game.gameObjects.wallAt(pos.PositionWDir(direction)) != -1) {
+				// Si la fuerza de caida es mayor o igual a 3, muere
 				if (fuerzaCaida >= 3) {
 					vivo = false;
+				// Si no, se reinicia la fuerza de caida
 				} else {
 					fuerzaCaida = 0;
+					direction = previousDirection;
 				}
-			} else {
+			}
+			// Si no hay una pared abajo, aumenta la fuerza de caida y se actualiza la posicion con la direccion
+			else {
 				fuerzaCaida++;
 				pos.update(direction);
 			}
 		}
+		// Si esta yendo a la derecha o izquierda
 		else if (direction == Direction.RIGHT || direction == Direction.LEFT) {
+			// Si hay una pared en la siguiente posicion, se cambia la direccion a la simetrica y se guarda la anterior
 			if (game.gameObjects.wallAt(pos.PositionWDir(direction)) != -1) {
+				previousDirection = direction;
 				direction.setSymmetric();
+			// Si no hay una pared a la izquierad o derecha, se actualiza la posicion con la direccion
 			} else {
-				this.pos.update(direction);
+				// Se actualiza la posicion con la direccion
+				pos.update(direction);
+				// Si no hay una pared abajo, se cambia la direccion a DOWN y se guarda la anterior
 				if (game.gameObjects.wallAt(pos.PositionWDir(Direction.DOWN)) == -1) {
+					previousDirection = direction;
 					direction = Direction.DOWN;
 				}
 			}
@@ -81,40 +97,40 @@ public class Lemming {
 	}
 
 	// Getters
-	//Funcion para obtener la posicion
-	public Position getPos() {
-		return new Position(this.pos.getCol(), this.pos.getRow());
-	}
+		//Funcion para obtener la posicion
+		public Position getPos() {
+			return new Position(this.pos.getCol(), this.pos.getRow());
+		}
 
-	//Funcion para obtener la direccion
-	public Direction getDirection() {
-		Direction newDirection = direction;
-		return newDirection;
-	}
+		//Funcion para obtener la direccion
+		public Direction getDirection() {
+			Direction newDirection = direction;
+			return newDirection;
+		}
 
-	//Funcion para obtener si esta vivo
-	public boolean isVivo() {
-		return vivo;
-	}
+		//Funcion para obtener si esta vivo
+		public boolean isVivo() {
+			return vivo;
+		}
 
-	// Funcion para obtener la fuerza de caida
-	public int getFuerzaCaida() {
-		return fuerzaCaida;
-	}
+		// Funcion para obtener la fuerza de caida
+		public int getFuerzaCaida() {
+			return fuerzaCaida;
+		}
 
-	// Funcion para obtener el rol
-	public WalkerRole getRol() {
-		return new WalkerRole();
-	}
+		// Funcion para obtener el rol
+		public WalkerRole getRol() {
+			return new WalkerRole();
+		}
 
-	// Funcion para obtener la proxima posicion
-	public Position nextPos() {
-		return pos.PositionWDir(direction);
-	}
+		// Funcion para obtener la proxima posicion
+		public Position nextPos() {
+			return pos.PositionWDir(direction);
+		}
 
-	// Funcion para obtener el juego
-	public Game getGame() {
-		return new Game(this.game.getLevel());
-	}
+		// Funcion para obtener el juego
+		public Game getGame() {
+			return new Game(this.game.getLevel());
+		}
 
 }
