d8a082488feba2429a4ab9cbbd59265a84762da4
Pablo Pardo <pablo.pc2005@gmail.com>
2024-10-16 20:50:23 +0200

    sangrado

--- Practica/src/tp1/logic/lemmingRoles/WalkerRole.java
+++ Practica/src/tp1/logic/lemmingRoles/WalkerRole.java
@@ -6,15 +6,16 @@ import tp1.view.Messages;
 
 public class WalkerRole {
 
-	 public void play(Lemming lemming) {
-		lemming.move();
-	 }
+	public void play(Lemming lemming) {
+	    lemming.move();
+	}
 
-	 public String getIcon(Lemming lemming) {
+	public String getIcon(Lemming lemming) {
         if (lemming.getDirection().equals(Direction.DOWN)) {
             if (lemming.getPreviousDirection().equals(Direction.LEFT)) return Messages.LEMMING_LEFT;
             else return Messages.LEMMING_RIGHT;
-        } else if (lemming.getDirection().equals(Direction.LEFT)) return Messages.LEMMING_LEFT;
+        } 
+        else if (lemming.getDirection().equals(Direction.LEFT)) return Messages.LEMMING_LEFT;
         else return Messages.LEMMING_RIGHT;
     }
 }
